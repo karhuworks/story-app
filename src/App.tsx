d0d8d799ed34e1fae62b222d24d315419e0db526
karhuworks <karhuworks@gmail.com>
2026-01-20 21:22:44 +0200

    set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Story App</h1>
+      <p>Writing & planning tool</p>
+      <p>testing testing</p>
+      <p>I have no idea what I am doing but I am winging it</p>
+      <p>NOW IT IS CENTERED HAHAA</p>
+    </div>
+  );
+}
+
+export default App;
